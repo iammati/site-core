466c8e25fd075654455bf2e68ceb6b2591179c94
Mati Sediqi <mati_01@icloud.com>
2021-01-10 03:21:58 +0100

    [NEW] CurlUtility

---
+++ Classes/Utility/CurlUtility.php
@@ -0,0 +1,82 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Site\Core\Utility;
+
+class CurlUtility
+{
+    /**
+     * @var resource|false|\CurlHandle
+     */
+    protected $curl = null;
+
+    /**
+     * @var string|bool
+     */
+    protected $response = false;
+
+    /**
+     * Retrieves the curl obejct.
+     * 
+     * @return resource|false|\CurlHandle
+     */
+    public function getCurl()
+    {
+        return $this->curl;
+    }
+
+    /**
+     * Retrieves the response of the executed cURL.
+     * 
+     * @return string|bool
+     */
+    public function getResponse()
+    {
+        return $this->response;
+    }
+
+    /**
+     * Executes a cURL request to the provided URI with additional query-params.
+     * Method does not supports post parameters. You might try out in such
+     * an use-case the executeParams-method.
+     * 
+     * @param string $uri
+     * @param array $params
+     * 
+     * @see executeParams
+     * 
+     * @return this
+     */
+    public function execute($uri)
+    {
+        // Get cURL resource
+        $curl = curl_init();
+
+        // Set some options - we are passing in a useragent too here
+        curl_setopt_array($curl, [
+            CURLOPT_SSL_VERIFYPEER => 0,
+            CURLOPT_RETURNTRANSFER => 1,
+            CURLOPT_URL => $uri,
+        ]);
+
+        // Send the request & save response to $resp
+        $resp = curl_exec($curl);
+        $this->response = $resp;
+
+        $this->curl = $curl;
+
+        // Close request to clear up some resources
+        curl_close($curl);
+
+        return $this;
+    }
+
+    /**
+     * @todo finish this
+     */
+    public function executeParams()
+    {
+
+    }
+}
