fe98b1515cdcaf7fd7b9c61b57430cc8546e048a
Mati Sediqi <mati_01@icloud.com>
2021-01-22 15:44:02 +0100

    Fix env

--- Classes/Composer/EnvLoader.php
+++ Classes/Composer/EnvLoader.php
@@ -37,6 +37,10 @@ class EnvLoader
             $rootPath .= '/'.$node;
         }
 
+        if (StrUtility::startsWith($rootPath, '//')) {
+            $rootPath = $_SERVER['DOCUMENT_ROOT'].'/'.$node;
+        }
+
         return $rootPath;
     }
 }
