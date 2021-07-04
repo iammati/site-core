708932b05fb754fd3312c54e0d6b373ecaf5111a
Mati Sediqi <mati_01@icloud.com>
2021-07-04 23:04:49 +0200

    [RELEASE] v1.8.6
    - Added Inline and InlineItem as TCAService fields

--- Classes/Form/Fields/Inline.php
+++ Classes/Form/Fields/Inline.php
@@ -3,16 +3,17 @@
 namespace Site\Core\Form\Fields;
 
 use Site\Core\Interfaces\FieldInterface;
+use Site\Core\Service\TCAService;
 
 class Inline extends Field implements FieldInterface
 {
     public static function make(string $label, array $fieldConfig = [])
     {
-        $fieldIdentifier = basename(__FILE__, '.php');
+        return TCAService::findConfigByType('Inline', basename(__FILE__, '.php'), '', [
+            'label' => $fieldConfig['label'],
+            'title' => $label,
 
-        return self::create($fieldIdentifier, [
-            'label' => $label,
-            'fieldConfig' => $fieldConfig,
+            'columns' => $fieldConfig['columns'],
         ]);
     }
 }
---
+++ Classes/Form/Fields/InlineItem.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Site\Core\Form\Fields;
+
+use Site\Core\Interfaces\FieldInterface;
+use Site\Core\Service\TCAService;
+
+class InlineItem extends Field implements FieldInterface
+{
+    public static function make(string $label, array $fieldConfig = [])
+    {
+        return TCAService::findConfigByType('InlineItem', '', $label, [
+            'foreign_table' => $fieldConfig['config']['foreign_table'],
+        ]);
+    }
+}
