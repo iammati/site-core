161a6f9ec1234d75dcd70ff645b4221eea5710fa
Mati Sediqi <mati_01@icloud.com>
2021-07-18 00:06:46 +0200

    [TASK] Prepared the extension for upcoming v3 for TYPO3 11.

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Site-Core',
     'description' => 'Core-related management here. Whether frontend or backend, Event or Middleware registration, RTE manipulation or anything else.',
-    'version' => '2.0.2',
+    'version' => '3.0.0',
     'category' => 'plugin',
     'author' => 'Mati',
     'author_email' => 'mati_01@icloud.com',
@@ -14,7 +14,7 @@ $EM_CONF[$_EXTKEY] = [
         'suggests' => [],
 
         'depends' => [
-            'typo3' => '10.4.17-10.4.99',
+            'typo3' => '11.3.00-11.3.99',
             'container' => '*',
         ],
     ],
