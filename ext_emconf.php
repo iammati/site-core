b65c15283697b8241c8be2b53f1ce53a914f16c8
Mati Sediqi <mati_01@icloud.com>
2021-08-26 19:06:39 +0200

    v2.2.3 fix-release

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Site-Core',
     'description' => 'Core-related management here. Whether frontend or backend, Event or Middleware registration, RTE manipulation or anything else.',
-    'version' => '2.2.1',
+    'version' => '2.2.3',
     'category' => 'plugin',
     'author' => 'Mati',
     'author_email' => 'mati_01@icloud.com',
@@ -14,7 +14,7 @@ $EM_CONF[$_EXTKEY] = [
         'suggests' => [],
 
         'depends' => [
-            'typo3' => '10.4.18-10.4.99',
+            'typo3' => '10.4.20-10.4.99',
             'container' => '*',
         ],
     ],
