3d5eb782f0637359ca68cd66c4f667fe95e4e41e
Mati Sediqi <m.sediqi@schommer-media.de>
2022-01-24 09:50:14 +0100

    [RELEASE] v2.2.11

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Site-Core',
     'description' => 'Core-related management here. Whether frontend or backend, Event or Middleware registration, RTE manipulation or anything else.',
-    'version' => '2.2.10',
+    'version' => '2.2.11',
     'category' => 'plugin',
     'author' => 'Mati',
     'author_email' => 'mati_01@icloud.com',
